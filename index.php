55e30af9c784a43ff6bc71cfb8322bf14e48bc70
ellen <queiroz.ellen@aluno.ifso.edu.br>
2022-08-24 12:11:43 -0300

    commit

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Página Inicial| Projeto para Web com PHP</title>
+        <link rel="stylesheet"
+              href="lib/bootstrap-4.2.1-dist/css/bootstrap-grid.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- TOPO//-->
+                    <?php
+                        include 'includes/topo.php';
+                        ?>
+                </div>
+            </div>
+            <div class="row" style="min-height: 500px;">
+                <div class="col-md-12">
+                    <!--menu//---> 
+                    <?php
+                        include 'includes/menu.php';
+                        ?>
+                </div>
+                <div class="col-md-10" style="padding-top: 550px;">
+                    <!--conteudo//-->
+                    <h2>Pagina Inicial </h2>,
+                    <?php
+                        include 'includes/busca.php'
+                    ?>
+                    
+                    <?php
+                        require_once 'includes/funcoes.php';
+                        require_once 'core/conexao_mysql.php';
+                        require_once 'core/sql.php';
+                        require_once 'core/mysql.php';
+
+                        foreach($_GET as $indice => $dados){
+                                $$indice = limparDados($dado);
+                        }
+
+                        $data_atual = date('Y-m-d h:i:s');
+
+                        $criterio = [
+                            ['data_postagem', '<=', $data_atual]
+                        ];
+
+                        if(!empty($busca)) {
+                            $criterio[] = [
+                                'AND',
+                                'texto',
+                                'like', 
+                                "%{$busca}%"
+                            ];
+                        }
+
+                        $posts = buscar(
+                            'post',
+                            [
+                                'titulo',
+                                'data_postagem',
+                                'id',
+                                '(select nome
+                                    from usuario
+                                    where usuario.id = post.usuario_id) as nome'
+                            ], 
+                            $criterio,
+                            'data_postagem DESC'
+                        );
+                    ?>
+                    
+                    <div>
+                        <div class="list-group">
+                            <?php
+                            foreach($posts as $post):
+                                $data = date_create($post['data_postagem']);
+                                $data = date_format($data,'d/m/Y H:i:s');
+                            ?>
+                            <a class="list-grouo-item list-group-item-action"
+                                href="post_detalhe.php?post=<?php echo $post['id1']?>">
+                                <strong><?php echo $post ['titulo']?></strong>
+                                [<?php echo $post['nome']?>]
+                                <span class="badge badge-dark"><?php echo $data?></span>
+                            </a>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <!--- rodapé //--> 
+                    <?php 
+                    include 'includes/rodape.php'; 
+                    ?>
+                </div>
+            </div>
+        </div>
+        <script src="lib/bootstrap-4.2.1-dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
