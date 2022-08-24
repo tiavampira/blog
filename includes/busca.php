8d934ba2fbbedf1787ca6a9236a308b250c011c7
ellen <queiroz.ellen@aluno.ifso.edu.br>
2022-08-24 11:20:35 -0300

    commit

--- includes/busca.php
+++ includes/busca.php
@@ -2,5 +2,5 @@
     <input class="form-control mr-sm-2" type="search" name="busca"
            placeholder="Busca" aria-label="Busca">
     <button class="btn btn-outline-success my-2 my-sm-0"
-            type="submit">Buscar</button>
+            type="submit"> Buscar</button>
 </form>
--- includes/menu.php
+++ includes/menu.php
@@ -5,23 +5,23 @@
     <div class="card-body">
         <ul class="nav flex-column">
             <li class="nav-link">
-                <a class="nav-link" href="index.php">Home </a>
+                <a class="nav-link" href="index.php"> Home </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="usuario_formulario.php">Cadastre-se"</a>
+                <a class="nav-link" href="usuario_formulario.php"> Cadastre-se</a>
             </li>
             <li class="nav-item">
-                <a class="nav-item" href="login_formulario.php">Login</a>
+                <a class="nav-item" href="login_formulario.php"> Login</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="post_formulario.php">Incluir Post </a>
+                <a class="nav-link" href="post_formulario.php"> Incluir Post </a>
             </li>
             <?php if((isset($_SESSION['login'])) 
-                        &&($_SESSION['login']['usuario']['adm']==1)) : ?>
+                        &&($_SESSION['login']['usuario']['adm']===1)) : ?>
             <li class="nav-item">
-                <a class="nav-link" href="usuarios.php">Usuarios</a>
+                <a class="nav-link" href="usuarios.php"> Usuarios</a>
             </li>
-            <?php endif?>
+            <?php endif; ?>
         </ul>
     </div>
 </div>
